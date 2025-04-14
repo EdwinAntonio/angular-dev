bfb2ac9a16bffffdf8035c66d3f15393d6c7ddaa
Edwin-Antonio~ <edanarellanoma@ittepic.edu.mx>
2025-04-14 02:41:44 -0600

    Signal concepts

--- Bases/src/app/counter/counter.component.ts
+++ Bases/src/app/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, signal, Signal } from "@angular/core";
 
 @Component({
   templateUrl: './counter.component.html',
@@ -7,10 +7,19 @@ import { Component } from "@angular/core";
 
 export class CounterComponent{
   counter = 10
+
+  /* Signals are only another way to use data in a quickly way instead the tradicional declarations
+     and you'll able to used with set(), update(), delete() declarations becuase Signal declarations
+     must to be used as FUNCTIONS always
+  */
+  counterSignal = signal(10)
+
   increaseBy(value : number){
     this.counter += value
+    this.counterSignal.update((current) => current + value)
   }
   restartCounter(){
     this.counter = 10
+    this.counterSignal.set(10)
   }
 }
