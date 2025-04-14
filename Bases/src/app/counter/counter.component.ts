943e48bee7aa72fe6099e91eda8018cc358853db
Edwin-Antonio~ <edanarellanoma@ittepic.edu.mx>
2025-04-14 15:47:30 -0600

    Zoneless practice

--- Bases/src/app/counter/counter.component.ts
+++ Bases/src/app/counter/counter.component.ts
@@ -1,25 +1,34 @@
-import { Component, signal, Signal } from "@angular/core";
+import { ConstantPool } from "@angular/compiler";
+import { ChangeDetectionStrategy, Component, signal, Signal } from "@angular/core";
 
 @Component({
   templateUrl: './counter.component.html',
-  styleUrl: './counter.component.css'
+  styleUrl: './counter.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 
 export class CounterComponent{
-  counter = 10
+  counter = 10;
+
+  constructor(){
+    setInterval(() => {
+      this.counterSignal.update(v => v + 1);
+      console.log('Tick');
+    }, 2000);
+  }
 
   /* Signals are only another way to use data in a quickly way instead the tradicional declarations
      and you'll able to used with set(), update(), delete() declarations becuase Signal declarations
      must to be used as FUNCTIONS always
   */
-  counterSignal = signal(10)
+  counterSignal = signal(10);
 
   increaseBy(value : number){
-    this.counter += value
-    this.counterSignal.update((current) => current + value)
+    this.counter += value;
+    this.counterSignal.update((current) => current + value);
   }
   restartCounter(){
-    this.counter = 10
-    this.counterSignal.set(10)
+    this.counter = 10;
+    this.counterSignal.set(10);
   }
 }
