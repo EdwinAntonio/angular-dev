d23c2b9e257bf63b7ee1adf1fdd750c5db70ac7c
Edwin-Antonio~ <edanarellanoma@ittepic.edu.mx>
2025-04-15 04:05:34 -0600

    New Dragonball page added

--- Bases/src/app/app.routes.ts
+++ Bases/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { CounterComponent } from './pages/counter/counter.component';
 import { HeroPageComponent } from './pages/hero/hero-page.component';
+import { DragonBallPageComponent } from './pages/dragonball/dragonball-page.component';
 
 export const routes: Routes = [
 
@@ -11,5 +12,13 @@ export const routes: Routes = [
   {
     path: 'hero',
     component: HeroPageComponent
+  },
+  {
+    path: 'dragonball',
+    component: DragonBallPageComponent
+  },
+  {
+    path: '**',  //Este tipo de Path nos permite que cuando el usuario meta una URL incorrecta lo redirija al path especifico
+    redirectTo: ''
   }
 ];
